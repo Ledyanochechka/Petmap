00dc62c0fb4ec5f3dda923e4afa526acfaa36186
daria <d.musarskaya@gmail.com>
2026-04-05 19:25:10 +0300

    исправление формата ввода почты. Доступны только: gmail.com", "yandex.ru", "mail.ru

--- forms/register_form.py
+++ forms/register_form.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField, EmailField
-from wtforms.validators import DataRequired
-
+from wtforms.validators import DataRequired, ValidationError
 
 class RegisterForm(FlaskForm):
     login = StringField('Имя пользователя', validators=[DataRequired()])
@@ -9,3 +8,13 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     submit = SubmitField('Зарегистрироваться')
+
+    def validate_email(self, field):
+        allowed_domains = ["gmail.com", "yandex.ru", "mail.ru"]
+        try:
+            domain = field.data.split("@")[1]
+        except IndexError:
+            raise ValidationError("Неверный формат ввода почты, убедитесь в правильности написания.")
+
+        if domain not in allowed_domains:
+            raise ValidationError("Неверный формат ввода почты, убедитесь в правильности написания.")
